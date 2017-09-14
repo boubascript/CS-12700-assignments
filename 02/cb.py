21478d6be886fdc5f743179b26bd6c16740231de
Boubacar Diallo <bouba@meme-machine.local>
2017-09-14 10:44:32 -0400

    finished codingbat problems

--- 02/cb.py
+++ 02/cb.py
@@ -17,4 +17,26 @@ def string_bits(str):
   return bits
 
 #lone_sum (prob/p143951)
+def lone_sum(a, b, c):
+  if (a==b and b==c and a==c):
+    return  0
+  elif (b==c):
+    return a 
+  elif(a == c):
+      return b 
+  elif (a==b) :
+    return c
+  else:
+    return a + b + c
+
+#string_splosion (prob/p118366)
+def string_splosion(str):
+  splosion = ""
+  for i in range(len(str)+1):
+    splosion += str[0:i]
+  return splosion
+
+#cigar_party (prob/p195669)
+def cigar_party(cigars, is_weekend):
+  return cigars >= 40 and ( is_weekend or cigars <= 60)
 
