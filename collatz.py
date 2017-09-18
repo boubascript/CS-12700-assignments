886625efd1ba41183ff82d8fd6956769ef3eb658
Boubacar Diallo <bouba@meme-machine.local>
2017-09-18 09:58:33 -0400

    collatz done

--- collatz.py
+++ collatz.py
@@ -1,2 +1,16 @@
-def collatz():
-    
+def collatz(n):
+    if (n % 2 == 0):
+        n //= 2
+    else:
+        n = 3*n + 1
+    return n
+
+def collatzSequence():
+    num = int(input("Enter Number: "))
+    print(num)
+    while num != 1:
+        num = collatz(num)
+        print(num)
+        
+        
+collatzSequence()
