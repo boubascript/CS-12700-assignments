33b50554bccfe83e5bd1dcee464a0b6417b0d14b
Boubacar Diallo <bouba@meme-machine.local>
2017-09-18 23:29:15 -0400

    piglatin a little advanced

--- 03/piglatin.py
+++ 03/piglatin.py
@@ -1,27 +1,57 @@
-punct = [".",",","?","!",":",";",""]
+punct = [".",",","?","!",":",";",")",""]
+vowels = ["a","A","E","e","i","I","O","o","u","u"]
+
 def piglatinify(s):
     s = s.split(" ")
     translated = ""
     for word in s:
-        if(len(word) > 1 and word[:1] not in ["a","A","E","e","i","I","O","o","u","u"]):
+        parentheses = False
+        if(word[:1] == "("):
+            word = word.replace("(","")
+            parentheses = True
+        if(len(word) > 1 and word[:1] not in vowels):
             word = word[1:] + word[:1] + "ay"
         elif (word not in punct):
-           word = word + "ay"
-        i = 0
+           word += "ay"
         puncs = ""
         shifted  = ""
+        i = 0
         for ltr in word:
             if ltr in punct:
                 punc = word[i]
                 puncs += punc
-                shifted = word.replace(punc,"",1)
             i += 1
-        word = shifted + puncs
+        word = word.replace("".join(puncs),"")
+        word += "".join(puncs)
+        if(parentheses):
+            word = "(" + word
         translated += word + " "
-    print(translated)
+    return translated
         
-    
-           
-piglatinify(""" Hello... This is pig latin my guy. We lit!! """)
+              
+print(piglatinify("""This sentence is in pig latin."""))
 
+
+def pigLatinTranslator():
+    tryagain = True
+    while(tryagain):
+        try:
+            filename = input("Enter file name or path to translate: ")
+            with open(filename) as file:
+                story = file.read()
+                story = story.split("\n")
+                for line in story:
+                    line = piglatinify(line)
+                    print(line)
+            print("--------------- \n\n")
+            redo = input("Enter to translate another file, type anything to quit.")
+            if(redo!= ""):
+                tryagain = False
+        except FileNotFoundError:
+            print("Error: This file doesn't exist! Try again.")
+            print("Hint: Use path pigFileTests/piglatintest.txt for now")
+            tryagain = True
         
+            
+
+pigLatinTranslator()
---
+++ lists.py
@@ -0,0 +1,8 @@
+l = []
+l2 = ['a','b','c','d']
+
+print(len(l2))
+print(l2[2])
+
+s = "everybody has a plan until they get punched in the mouth"
+print(s.split())
