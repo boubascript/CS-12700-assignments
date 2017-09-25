6a7fcc7932f8aab377b6d341b3665620688e6a68
Boubacar Diallo <bouba@meme-machine.local>
2017-09-25 06:51:50 -0400

    madlibs pair programming with a regex twist

--- 04/madlibs.py
+++ 04/madlibs.py
@@ -1,4 +1,5 @@
-import random 
+import random
+import re
 
 story = """ There once was a NOUN who VERB. He was very ADJECTIVE. He VERB very well.   """
 nouns = [ "chair",  "head", "desk", "table", "tree", "paper", "towel","file" ]
@@ -19,7 +20,6 @@ keywords = ["NOUN", "VERB", "ADJECTIVE"]
 
 def convert(word):
     global nouns, verbs, adjectives
-    print(word)
     if "NOUN" == word:
         return random.choice(nouns)
     elif "VERB" == word:
@@ -32,7 +32,27 @@ def convert(word):
 
 def madlibs():
     global story
+    # Prepare for search
+    orig = story.strip()
 
+    # Search for the words to replace
+    # Returns match objects which we inspect
+    matches = re.finditer(r'NOUN|VERB|ADJECTIVE', orig)
+
+    # Start of unmatched section we're working on
+    start = 0
+
+    # Final string
+    s = ""
+
+    for x in matches:
+        span = x.span()  # Tuple with the start and end indexes of the match
+        new = convert(orig[span[0]:span[1]])  # Convert match to the proper random word
+        s += orig[start:span[0]] + new  # Add the section we skipped and the replaced word to the ending string
+        start = span[1]  # Mark the end of this match as the beginning of the next skipped section
+    s += orig[start:]  # Add any remaining bits of the string
+
+    return s  # return said string
     orig = story.strip()
 
     result = re.finditer(r'NOUN|VERB|ADJECTIVE', orig)
