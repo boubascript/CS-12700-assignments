41e115df8e745a117bb6ec96954bf04d1742f231
Boubacar Diallo <bouba@meme-machine.local>
2017-09-25 10:22:17 -0400

    functions should return because good style

--- 04/characterPictureGrid.py
+++ 04/characterPictureGrid.py
@@ -8,14 +8,15 @@ gridTest = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
-def printGrid(grid):
+def grid2string(grid):
+    result = ""
     for col in range(len(grid[0])):
         line = ""
         for row in range(len(grid)):
             line += grid[row][col]
-        print(line)
-        
-printGrid(gridTest)
+        result += line + "\n"
+    return result
+print(grid2string(gridTest))
 
 
     
