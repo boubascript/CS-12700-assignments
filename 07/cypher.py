9875a051eda794462506781b286913b13ac61a54
Boubacar Diallo <bouba@meme-machine.local>
2017-10-12 07:16:59 -0400

    fixed up and finished ciphers(used text file to create real stats)

--- 07/cypher.py
+++ 07/cypher.py
@@ -1,10 +1,35 @@
 import math
-alphabet = {"a":0.08167 , "b":0.01492, "c":0.02782 , "d":0.04253 , "e": 0.12702 , "f":0.02228 , "g":0.02015 , "h":0.06094,
+alphabet = {'a':0 , "b":0, "c":0 , "d":0, "e": 0 , "f":0 , "g":0 , "h":0,
             "i":0 , "j":0 , "k":0 , "l":0 , "m":0 , "n":0 , "o":0 , "p":0 , "q":0 , "r":0,
             "s":0 , "t":0 , "u":0 , "v":0 , "w":0 , "x":0 , "y":0 , "z":0 }
 alpha = list(alphabet.keys())
-real_stats = list(alphabet.values())
+#real_stats = [0.08167,0.01492,0.02782,0.04253,0.12702,0.02228,0.02015,0.06094,0.06966,0.00153,0.00772,0.04025,0.02406,0.06749,0.07507,0.01929,0.00095,0.05987,0.06327,0.09056,0.02758,0.00978,0.02360,0.00150,0.01974,0.00074]
 puncts = [" ",".",";",":",",","?","!","(",")"]
+symbols = ["@","#","$","%","^","&","*","+","-","_","~"]
+
+#create the frequencies that will be compared to as the 'real' values
+def create_stats(text):
+    total = 0
+    try:
+        with open(text) as f:
+            msg = f.read().lower()
+            msgWords = msg.split("\n")
+            for line in msgWords:
+                notcount = 0
+                for c in line:
+                    if c in puncts or c in symbols:
+                        notcount +=1
+                total += (len(line) - notcount)
+                for key,val in alphabet.items():
+                    alphabet[key] = val + line.count(key)
+            for key,val in alphabet.items():
+                alphabet[key] = val/total
+            return list(alphabet.values())
+    except FileNotFoundError:
+        print("Error: This file doesn't exist! Try again.")
+        
+# I used hitchikers guide to the galaxy as the text for reference
+real_stats = create_stats("hhgttg.txt")
 
 #My own Helper Functions--------
 def isLower(c):
@@ -68,12 +93,8 @@ def file_encode(file,r):
 
 def full_file_encode(file):
     return full_encode(file_encode(file,0))
-    
-print(full_encode("Alert: This message encrypted with a weak cypher!"))
 
-print(full_file_encode("cyphertest.txt"))
-
-
+#returns smaller of 2 lists
 def smaller(l1,l2):
     if(len(l1) > len(l2)):
         return l2
@@ -86,7 +107,6 @@ def distance(v1,v2):
         sum += (v2[i]-v1[i]) * (v2[i]-v1[i])
     sum = math.sqrt(sum)
     return sum
-
 
 def build_frequency_vector(s):
     notcount = 0
@@ -119,8 +139,13 @@ def decode(s):
         d = distance(freq,real_stats)
         distances.append(d)
     return allrotations[distances.index(min(distances))]
+     
 
-v2 = build_frequency_vector("hello")      
-#print(distance(real_stats,v2))
-
-print(full_encode("DwsDagmwhziArpmogWaSmmckwhMoEsmgmxlivpDttfjbjdxqBwxbKbCwgwgUyam"))
+def testcypher():
+    #print(real_stats)
+    encoded = file_encode("cyphertest.txt",23)
+    decoded = decode(encoded)
+    print("Encoded: " + encoded)
+    print("decoded: " + decoded)
+    
+testcypher()
